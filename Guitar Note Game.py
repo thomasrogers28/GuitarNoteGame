8cdcdc97ea9068414cad89daad48af3e2160a8a9
thomasrogers28 <96743747+thomasrogers28@users.noreply.github.com>
2021-12-30 12:24:35 +0000

    Loops
    Added loop to allow for multiple guesses per execution (currently set to 5 for testing)
    Added final score to show how well you did
    Updated "0th fret" to "open fret"

--- Guitar Note Game.py
+++ Guitar Note Game.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec 27 18:25:38 2021
-
-@author: Admin
+Last Edited Thu Dec 30 12:21:05 2021
+@author: Thomas Rogers
 """
 from random import randrange
 
@@ -37,62 +37,82 @@ finished_grid =  [["","","","","",""]
                 ,["","","","","",""]
                 ,["","","","","",""]];
 
-#pick a random fret and string to guess against
-string = randrange(6);
-fret = randrange(13);
+guess_scores = [];
 
-#convert to text for the pop up
-#since element 1 = string 6, we also need to flip the string number
-string_str = str(6 - string);
-fret_str = str(fret);
+#this is to allow a max round number (to avoid having to complete 72 iterations each test)
+count = 1
 
-#create a suffix to show 1st, 2nd, 3rd or nth for string
-if string_str in ("1"):
-    string_suffix = "st"
-elif string_str in ("2"):
-    string_suffix = "nd"
-elif string_str in ("3"):
-    string_suffix = "rd"
-else:
-    string_suffix = "th";
-
-#create a suffix to show 1st, 2nd, 3rd or nth for fret    
-if fret_str in ("1"):
-    fret_suffix = "st"
-elif fret_str in ("2"):
-    fret_suffix = "nd"
-elif fret_str in ("3"):
-    fret_suffix = "rd"
-else:
-    fret_suffix = "th";
-
-#pop up the question
-question = string_str + string_suffix + " string, " + fret_str + fret_suffix + " fret: ";
-
-#submit guess
-guess = input(question);
-
-#fix formatting of the guess
-guess = guess.capitalize();
-
-#change flats to sharps to match the answer grid
-if len(guess) == 2 and guess[1] == "b" and guess[0] == "A":
-    guess = "G#"
-elif len(guess) == 2 and guess[1] == "b":
-    guess = chr(ord(guess[0]) - 1) + "#";
+#loop until finished_grid is complete
+while (answer_grid != finished_grid) and count <= 5:
+    #pick a random fret and string to guess against
+    compare = "H";
+    while compare != "":
+        string = randrange(6);
+        fret = randrange(13);
+        compare = finished_grid[fret][string];
     
-#print the guess for trouble shooting
-print(guess);
-
-#find answer from starting grid
-answer = answer_grid[fret][string];
-
-#show if the answer is correct
-if guess == answer:
-    print("Correct!")
-else:
-    print("Incorrect - The answer is " + answer);
-
-#update blank grid
-finished_grid[fret][string] = answer;
+    #convert to text for the pop up
+    #since element 1 = string 6, we also need to flip the string number
+    string_str = str(6 - string);
+    fret_str = str(fret);
+    
+    #create a suffix to show 1st, 2nd, 3rd or nth for string
+    if string_str in ("1"):
+        string_suffix = "st"
+    elif string_str in ("2"):
+        string_suffix = "nd"
+    elif string_str in ("3"):
+        string_suffix = "rd"
+    else:
+        string_suffix = "th";
+    
+    #create a suffix to show open, 1st, 2nd, 3rd or nth for fret    
+    if fret_str in ("0"):
+        fret_suffix = "open"
+    elif fret_str in ("1"):
+        fret_suffix = fret_str + "st"
+    elif fret_str in ("2"):
+        fret_suffix = fret_str + "nd"
+    elif fret_str in ("3"):
+        fret_suffix = fret_str + "rd"
+    else:
+        fret_suffix = fret_str + "th";
+    
+    #pop up the question
+    question = string_str + string_suffix + " string, " + fret_suffix + " fret: ";
+    
+    #submit guess
+    guess = input(question);
+    
+    #fix formatting of the guess
+    guess = guess.capitalize();
+    
+    #change flats to sharps to match the answer grid
+    if len(guess) == 2 and guess[1] == "b" and guess[0] == "A":
+        guess = "G#"
+    elif len(guess) == 2 and guess[1] == "b":
+        guess = chr(ord(guess[0]) - 1) + "#";
+        
+    #print the guess for trouble shooting
+    print(guess);
+    
+    #find answer from starting grid
+    answer = answer_grid[fret][string];
+    
+    #show if the answer is correct
+    if guess == answer:
+        print("Correct!")
+        guess_scores.append(1);
+    else:
+        print("Incorrect - The answer is " + answer)
+        guess_scores.append(0);
+    
+    #update blank grid
+    finished_grid[fret][string] = answer;
+    
+    count = count + 1;
+ 
+#display results  
+print("Congratulations, you've scored:")  
+print(100 * sum(guess_scores)/len(guess_scores));
 
